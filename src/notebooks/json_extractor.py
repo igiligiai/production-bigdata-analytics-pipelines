11051922abc70cae16287569a5e2937670277882
Anthony Igiligi <anthony.igiligi@sii-germany.com>
2026-05-13 15:39:33 +0200

    updated implementation

---
+++ src/notebooks/json_extractor.py
@@ -0,0 +1,42 @@
+# Databricks notebook source
+
+# COMMAND ----------
+
+import json
+from utils.stock_prices import extract_company_info, extract_hourly_prices
+
+# COMMAND ----------
+
+BASE_DIR = "dbfs:/raw/depeap/extract"
+
+# COMMAND ----------
+
+def write_hourly_data():
+    subdir = f"{BASE_DIR}/hourly_data"
+    hourly_data = extract_hourly_prices()
+    extracted_at = hourly_data[0].get('extracted_at')
+    date = extracted_at.split(' ')[0]
+    hour = extracted_at.split(' ')[1]
+    file_name = f"{hour.replace(':','_')}.json"
+    dbutils.fs.mkdirs(f"{subdir}/{date}")
+    file_path = f"{subdir}/{date}/{file_name}"
+    with open(file_path.replace("dbfs:", "/dbfs/"), 'w') as f:
+        json.dump(hourly_data, f)
+
+# COMMAND ----------
+
+def write_company_info():
+    subdir = f"{BASE_DIR}/company_info"
+    company_info = extract_company_info()
+    file_path = f"{subdir}/company_info.json"
+    dbutils.fs.mkdirs(subdir)
+    with open(file_path.replace("dbfs:", "/dbfs/"), 'w') as f:
+        json.dump(company_info, f)
+
+# COMMAND ----------
+
+write_hourly_data()
+
+# COMMAND ----------
+
+write_company_info()
